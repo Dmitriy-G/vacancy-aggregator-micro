fd550d768ea3c1f6c1456280f63365cc86cf391d
Dmitriy Gorokhovtsev <dgsubscr23@gmail.com>
2020-10-25 20:11:38 +0300

    add core service

---
+++ vacancy_aggregator_core/src/main/java/com/vacancy/vacancy_aggregator_core/VacancyAggregatorCoreApplication.java
@@ -0,0 +1,13 @@
+package com.vacancy.vacancy_aggregator_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyAggregatorCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacancyAggregatorCoreApplication.class, args);
+    }
+
+}
---
+++ vacancy_aggregator_core/src/test/java/com/vacancy/vacancy_aggregator_core/VacancyAggregatorCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacancy.vacancy_aggregator_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyAggregatorCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
