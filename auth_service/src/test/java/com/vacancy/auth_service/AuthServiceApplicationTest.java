ec3718081aaeed3aeedaa98da8e01ff2a17813d9
Dmitriy Gorokhovtsev <dgsubscr23@gmail.com>
2020-11-01 01:05:56 +0300

    auth service add blank test

---
+++ auth_service/src/test/java/com/vacancy/auth_service/AuthServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.vacancy.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplicationTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplicationTest.class, args);
+    }
+
+}
